a77610d3e5fdf7004cddde3f5f3cd47e5b99e905
prakash <prakash.gbpec@gmail.com>
2015-04-12 00:20:08 +0530

    unused files deleted

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# 91springboard_hackathon
+This flask app is in development phase. To run this
+
+1. Install flask.
+2. Make a virual environment with virtualenv and install all requirements in
+   requirements.txt file.
+3. In the root folder run. "python manage.py runserver"
--- app/hacks/routes.py
+++ app/hacks/routes.py
@@ -1,15 +1,15 @@
 from flask import render_template
 from . import hacks
 import googlemaps
-from googleplaces import GooglePlaces, types, lang
+from googleplaces import GooglePlaces
 import collections
 @hacks.route('/')
-def index1():
+def index():
     data=[]
 	#import pdb; pdb.set_trace()
     position="mahipalpur, new delhi"
-    gmaps = googlemaps.Client(key='AIzaSyB3wNLhjT9wEyeFvSN1yjp3iEdTFp76pt8')
-    google_places = GooglePlaces('AIzaSyB3wNLhjT9wEyeFvSN1yjp3iEdTFp76pt8')
+    gmaps = googlemaps.Client(key='Add your own api key')
+    google_places = GooglePlaces('Add your own api key')
     query_result = google_places.nearby_search(location=position, keyword='public toilets',radius=20000)
     for data1 in query_result.places:
             matrix=gmaps.distance_matrix("%s, %s " %(str(data1.geo_location['lat']),str(data1.geo_location['lng'])),position)
@@ -27,7 +27,7 @@ def index1():
                             distance=elements['distance']
                             if int(distance['value'])<int(x['value']):
                                             x=distance
-    return render_template('hacks/index1.html',data=x, location=position)
+    return render_template('hacks/index.html',data=x, location=position)
 
 
 
